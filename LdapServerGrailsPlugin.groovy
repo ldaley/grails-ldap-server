9ac350a20767e793991108fdba269680cf34806b
ldaley <ldaley@832c1c66-4827-0410-8465-ccb17913cd09>
2009-05-19 23:50:32 +0000

    • Cleaned up whitespace.

--- LdapServerGrailsPlugin.groovy
+++ LdapServerGrailsPlugin.groovy
@@ -2,34 +2,34 @@ import grails.ldap.server.TransientGrailsLdapServer
 
 class LdapServerGrailsPlugin {
 
-    def version = "0.1"
-    def grailsVersion = "1.1 > *"
-    def dependsOn = [:]
-    def pluginExcludes = []
+	def version = "0.1"
+	def grailsVersion = "1.1 > *"
+	def dependsOn = [:]
+	def pluginExcludes = []
 
-    def author = "Luke Daley"
-    def authorEmail = "ld@ldaley.com"
-    def title = "Embedded LDAP Server Plugin"
-    def description = 'Allows the embedding of an LDAP directory (via ApacheDS) for testing purposes'
+	def author = "Luke Daley"
+	def authorEmail = "ld@ldaley.com"
+	def title = "Embedded LDAP Server Plugin"
+	def description = 'Allows the embedding of an LDAP directory (via ApacheDS) for testing purposes'
 
-    def documentation = "http://grails.org/plugin/grails-ldap-server"
+	def documentation = "http://grails.org/plugin/grails-ldap-server"
 
 	def servers = []
 	
-    def doWithSpring = {
+	def doWithSpring = {
 		createServers(application.config.ldapServers, delegate)
-    }
+	}
 
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
+	def onChange = { event ->
+		// TODO Implement code that is executed when any artefact that this plugin is
+		// watching is modified and reloaded. The event contains: event.source,
+		// event.application, event.manager, event.ctx, and event.plugin.
+	}
 
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
+	def onConfigChange = { event ->
+		// TODO Implement code that is executed when the project configuration changes.
+		// The event is the same as for 'onChange'.
+	}
 
 	def createServers(config, beanBuilder) {
 		config.each { name, props ->
