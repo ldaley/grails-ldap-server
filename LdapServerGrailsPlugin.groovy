1f1e72aa640c9b8c80b717fc9a716a7125430ef3
burtbeckwith <burtbeckwith@832c1c66-4827-0410-8465-ccb17913cd09>
2010-06-14 15:37:01 +0000

    0.1.8 release

--- LdapServerGrailsPlugin.groovy
+++ LdapServerGrailsPlugin.groovy
@@ -3,22 +3,22 @@ import grails.ldap.server.TransientGrailsLdapServer
 class LdapServerGrailsPlugin {
 
 	static beanNameSuffix = "LdapServer"
-	
-	def version = "0.1.7"
+
+	def version = "0.1.8"
 	def grailsVersion = "1.1 > *"
 	def dependsOn = [:]
 	def watchedResources = ["file:./grails-app/ldap-servers/*/data/*.ldif", "file:./grails-app/ldap-servers/*/schema/*.ldif"]
-	
+
 	def author = "Luke Daley"
 	def authorEmail = "ld@ldaley.com"
 	def title = "Embedded LDAP Server Plugin"
 	def description = 'Allows the embedding of an LDAP directory (via ApacheDS) for testing purposes'
 	def documentation = "http://grails.org/plugin/grails-ldap-server"
-	
+
 	def pluginExcludes = ["grails-app/ldap-servers/**"]
-	
+
 	def servers = []
-	
+
 	def doWithSpring = {
 		createServers(application.config.ldapServers, delegate)
 	}
@@ -34,22 +34,22 @@ class LdapServerGrailsPlugin {
 	}
 
 	def handleChange = { event ->
-		servers.each { 
+		servers.each {
 			event.ctx.getBean(it).stop()
 			event.ctx.removeBeanDefinition(it)
 		}
 		servers.clear()
-		
+
 		def beanDefinitions = beans {
 			createServers(event.application.config.ldapServers, delegate)
 		}
-		
+
 		servers.each {
 			event.ctx.registerBeanDefinition(it, beanDefinitions.getBeanDefinition(it))
 			event.ctx.getBean(it).afterPropertiesSet()
 		}
 	}
-	
+
 	def createServers(config, beanBuilder) {
 		config.each { name, props ->
 			def beanName = name + beanNameSuffix
