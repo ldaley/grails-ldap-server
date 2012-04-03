d1b22a2d5004cfdc1908abf8f3d36234a3736656
Luke Daley <ld@ldaley.com>
2012-04-03 12:40:02 +0100

    Use dependency management.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,15 +1,20 @@
 grails.project.dependency.resolution = {
 	inherits( "global" )
 	repositories {
-		grailsCentral()
-		grailsHome()
-		mavenLocal()
 		mavenCentral()
-		grailsRepo "http://grails.org/plugins"
+		grailsCentral()
+	}
+
+	dependencies {
+		compile 'org.apache.directory.server:apacheds-core:1.5.4'
+		compile 'org.apache.directory.server:apacheds-protocol-ldap:1.5.4'
 	}
 	
 	plugins {
-		build (":release:1.0.1") {
+		build (":release:1.0.1", ":svn:1.0.2") {
+			export = false
+		}
+		test (":hibernate:2.0.1", ":tomcat:2.0.1") {
 			export = false
 		}
 	}
